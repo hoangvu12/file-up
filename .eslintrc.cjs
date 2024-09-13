dcacc9cc2893b8b1f8fd0c731bae2418d111c1f5
hoangvu12 <hggaming91@gmail.com>
2024-09-13 20:51:17 +0700

    initial commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -34,7 +34,9 @@ module.exports = {
         "plugin:react/jsx-runtime",
         "plugin:react-hooks/recommended",
         "plugin:jsx-a11y/recommended",
+        "plugin:tailwindcss/recommended",
       ],
+
       settings: {
         react: {
           version: "detect",
