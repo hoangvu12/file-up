311a230421c474aa19f762e1fa59d0854a9cde1b
hoangvu12 <hggaming91@gmail.com>
2024-09-17 06:57:33 +0700

    style: change fonts

--- app/root.tsx
+++ app/root.tsx
@@ -38,7 +38,7 @@ export const links: LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Balsamiq+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap",
   },
 ];
 
